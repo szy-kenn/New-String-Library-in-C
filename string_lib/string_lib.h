c9106dbd8d9e3518cae5c1f09cdeeec1853ea90c
szy-kenn <johnkennethvelano@gmail.com>
2023-03-12 13:37:04 +0800

    strlength will no longer increment by 1, added concat function

--- string_lib/string_lib.h
+++ string_lib/string_lib.h
@@ -9,16 +9,18 @@ size_t strlength(char *_Source);
 
 char* strcopy(char *_Source);
 
+char* concat(char *_firstString, char *_secondString);
+
 size_t strlength(char *_Source) {
 
     /**
-     * Returns the size of a string from a character pointer including the null character,
+     * Returns the size of a string from a character pointer excluding the null character,
      * 
      * It will first store the pointer, then store the initial memory location of it
      * before iterating through the string until it reaches the null terminator.
      * 
      * Once it reaches the null character, it will then return the difference between
-     * the temp's final and initial location, incremented by 1 to give space to the null character.
+     * the temp's final and initial location.
     */
 
     char *temp = _Source;
@@ -44,7 +46,7 @@ char* strcopy(char *_Source) {
     
     char *temp = _Source;
 
-    char *result = malloc(sizeof(char) * strlength(_Source));
+    char *result = malloc(sizeof(char) * strlength(_Source) + 1);
 
     int pos = 0;
 
@@ -56,6 +58,30 @@ char* strcopy(char *_Source) {
     }
 
     result[pos] = '\0';
+    return result;
+}
+
+char* concat(char *_firstString, char *_secondString) {
+
+    char *result = malloc(sizeof(char) * (strlength(_firstString) + strlength(_secondString)) + 1);
+
+    int res_position = 0, temp_position = 0;
+
+    char *temp[] = {_firstString, _secondString};
+
+    for (int i = 0; i < 2; i++) {
+
+        while (temp[i][temp_position] != '\0') {
+            result[res_position] = temp[i][temp_position];
+            res_position++;
+            temp_position++;
+        }
+
+        temp_position = 0;
+    }
+
+    result[res_position] = '\0';
+
     return result;
 }
 
